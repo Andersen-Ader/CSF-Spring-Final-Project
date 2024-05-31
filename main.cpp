855bdc349738d3fdaea271c832f1fb6a36996422
Studbug <nskkid@gmail.com>
2024-05-31 11:36:52 -0700

    Added include for new tank class to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include "tank.cc" //Tank(string identifier, string name, string armament, string nation, string bday);
 
 int main (int argc, char *argv[]) {
   std::cout << "Hello, World!" << std::endl;
+
   return 0;
 }
