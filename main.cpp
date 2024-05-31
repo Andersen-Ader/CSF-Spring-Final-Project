ed59c2ba6946fc014ec1328bfc1e57aeecac0c08
Studbug <nskkid@gmail.com>
2024-05-31 11:42:44 -0700

    Ranamed tank.cc to tank.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "tank.cc" //Tank(string identifier, string name, string armament, string nation, string bday);
+#include "tank.cpp" //Tank(string identifier, string name, string armament, string nation, string bday);
 
 int main (int argc, char *argv[]) {
   std::cout << "Hello, World!" << std::endl;
