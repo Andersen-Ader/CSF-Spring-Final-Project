8959516ef9d6834d8381175eba9bad909e1bcb5c
Andersen-Ader <work+gh@andersenader.com>
2024-05-31 12:13:10 -0700

    Updated main

--- main.cpp
+++ main.cpp
@@ -1,20 +1,26 @@
 #include <iostream>
 #include "tank.cpp" //Tank(string identifier, string name, string armament, string nation, string bday);
+#include <string>
 
 
-void New();
-void Print();
-void Remove();
-void Save();
-void Quit();
+
+void New();    //adds a new entry to the list
+void Print();  //prints a node based on the given identifier
+void Remove(); //removes a node based on the given identifier
+void Save();   //saves the current linked list to a file
+void Quit();   //saves the linked list and exits the program
 
 
 int main (int argc, char *argv[]) {
-  
+  std::string userCommand;
+
   while (true) {
     std::cout << "> ";
     std::cin >> userCommand;
-    
+     if (userCommand.compare("New")) {
+      std::cout << "Works";
+      break;
+     }
   }
   return 0;
 }
