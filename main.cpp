4d1fd5ff97e194c76a131c1a0f200dd82fad4774
Andersen-Ader <work+gh@andersenader.com>
2024-05-31 11:55:53 -0700

    Updated main

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 #include "tank.cpp" //Tank(string identifier, string name, string armament, string nation, string bday);
 
-int main (int argc, char *argv[]) {
-  std::cout << "Hello, World!" << std::endl;
 
+void New();
+void Print();
+void Remove();
+void Save();
+void Quit();
+
+
+int main (int argc, char *argv[]) {
+  
+  while (true) {
+    std::cout << "> ";
+    std::cin >> userCommand;
+    
+  }
   return 0;
 }
