a008d51023a9b73e6f3615266d4bee811d22b1c2
Andersen-Ader <work+gh@andersenader.com>
2024-05-31 12:49:26 -0700

    Updated main

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "tank.cpp" //Tank(string identifier, string name, string armament, string nation, string bday);
 #include <string>
 
-
+using namespace std;
 
 void New();    //adds a new entry to the list
 void Print();  //prints a node based on the given identifier
@@ -12,15 +12,57 @@ void Quit();   //saves the linked list and exits the program
 
 
 int main (int argc, char *argv[]) {
-  std::string userCommand;
+  string userCommand;
 
   while (true) {
-    std::cout << "> ";
-    std::cin >> userCommand;
-     if (userCommand.compare("New\n")) {
-      std::cout << "Works";
+    cout << "> ";
+    cin >> userCommand;
+    if (userCommand.compare("New\n")) {
+      New();
+    }
+    if (userCommand.compare("Print\n")) {
+      Print();
+    }
+    if (userCommand.compare("Remove\n")) {
+      Remove();
+    }
+    if (userCommand.compare("Save\n")) {
+      Save();
+    }
+    if (userCommand.compare("Quit\n")) {
+      Save();
       break;
-     }
+    }
   }
   return 0;
 }
+
+
+void New() {
+  string identifier;
+  string name;
+  string armament;
+  string nation;
+  string bday;
+  
+  cout << "Enter identifier : ";
+  cin >> identifier;
+  cout << "Enter name : ";
+  cin >> name;
+  cout << "Enter armament : ";
+  cin >> armament;
+  cout << "Enter nation : ";
+  cin >> nation;
+  cout << "Enter Birthday : ";
+  cin >> bday;
+
+
+}
+
+void Print() {
+
+}
+
+void Remove()
+void Save()
+void Quit()
