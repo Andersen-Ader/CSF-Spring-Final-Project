311f1643e6dbeef060c22e95e56ba933d82ba0e2
Andersen Ader <100447236+Andersen-Ader@users.noreply.github.com>
2024-06-04 05:33:29 -0700

    Create TheOneFile.cpp
    This is a mostly functional implementation of all current code in one file.

---
+++ TheOneFile.cpp
@@ -0,0 +1,252 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+class Tank {
+    private:
+        string m_name;
+        string m_identifier;
+        string m_armament; 
+        string m_nation;
+        string m_bday;
+        
+    public:
+        Tank();
+        Tank(string identifier, string name, string armament, string nation, string bday);
+        void print();
+        void filePrint(ostream &out);
+        string getIdentifier();
+        string getName();
+        string getArmament();
+        string getNation();
+        string getBday();
+};
+
+class Node { 
+    public: 
+        Tank m_tank; 
+        Node* m_next; 
+  
+        Node() { 
+            m_tank = Tank("Null", "Null", "Null", "Null", "Null"); 
+            m_next = nullptr; 
+        } 
+  
+        Node(Tank tank) { 
+            m_tank = tank; 
+            m_next = nullptr; 
+        }
+
+        Node* getNext() {
+            return m_next;
+    } 
+};
+
+class TankList {
+    private:
+        Node* m_head;
+        Node* m_tail;
+
+    public:
+        TankList();
+        TankList(const Tank tankarray[], int size);
+        TankList(const string FILENAME);
+        Node* getHead();
+        Node* getTail();
+        Tank* searchFor(string target);
+        void pushBack(const Tank newItem);
+        void pushBack(const Tank tankarray[], int size);
+        void pushFront(const Tank newItem);
+        void insertAfter(const Tank target);
+        void remove(const Tank target);
+        
+};
+
+void New();           //adds a new entry to the list
+void Print();         //prints a node based on the given identifier
+void Remove();        //removes a node based on the given identifier
+void Save();          //saves the current linked list to a file
+void Quit();          //saves the linked list and exits the program
+
+int main (int argc, char *argv[]) {
+  string userCommand;
+
+  while (true) {
+    cout << "> ";
+    cin >> userCommand;
+    if (userCommand.compare("New\n")) {
+      New();
+    }
+    if (userCommand.compare("Print\n")) {
+      Print();
+    }
+    if (userCommand.compare("Remove\n")) {
+      Remove();
+    }
+    if (userCommand.compare("Save\n")) {
+      Save();
+    }
+    if (userCommand.compare("Quit\n")) {
+      Save();
+      break;
+    }
+  }
+  return 0;
+}
+
+void New() {
+    return;
+}
+
+void Print() {
+    return;
+}
+
+void Remove() {
+    return;
+} 
+
+void Save() {
+    return;
+}
+
+void Quit() {
+    return;
+}
+
+Tank::Tank() {
+    string nullStr = "Null";
+    m_identifier = nullStr;
+    m_name = nullStr;
+    m_armament = nullStr;
+    m_nation = nullStr;
+    m_bday = nullStr;
+}
+
+Tank::Tank(string identifier, string name, string armament, string nation, string bday) {
+    m_identifier = identifier;
+    m_name = name;
+    m_armament = armament;
+    m_nation = nation;
+    m_bday = bday;
+}
+
+void Tank::print() {
+    cout << "___" << endl << endl;
+    cout << "Identifier - \"" << m_identifier << "\"" << endl;
+    cout << "Name - \"" << m_name << "\"" << endl;
+    cout << "Armament - \"" << m_armament << "\"" << endl;
+    cout << "Nation - \"" << m_nation << "\"" << endl;
+    cout << "Birthday - \"" << m_bday << "\"" << endl << endl;
+    cout << "___" << endl;
+}
+
+void Tank::filePrint(ostream &out) {
+  out << m_identifier << " " << m_name << " " << m_armament << " " << m_nation << " " << m_bday << " " << endl;
+}
+
+string Tank::getIdentifier() {
+    return m_identifier;
+}
+
+string Tank::getName() {
+    return m_name;
+}
+
+string Tank::getArmament() {
+    return m_armament;
+}
+
+string Tank::getNation() {
+    return m_nation;
+}
+
+string Tank::getBday() {
+    return m_bday;
+}
+
+TankList::TankList() {
+    m_head = nullptr;
+    m_tail = nullptr;
+}
+/*
+TankList::TankList(const Tank tankarray[], int size) {
+    //Set the head node to the first item in the array
+    m_head = new Node(tankarray[0]);
+    Node* currNode = m_head;
+    for (int i = 1; i < size-1; i++) {
+        Node* nextNode = new Node(tankarray[i]);
+        currNode -> m_next = nextNode;
+        currNode = nextNode;
+    }
+    //Set tail for this list as the last item in the array
+    m_tail = new Node(tankarray[size-1]);
+    //Set the last node from the loop above's next node to the tail node
+    currNode -> m_next = m_tail;
+}
+*/ 
+TankList::TankList(const string FILENAME) {
+  string identifier, name, armament, nation, bday;
+  ifstream tanksFile("tanks.txt");
+
+  tanksFile >> identifier >> name >> armament >> nation >> bday;
+
+  Tank tank(identifier, name, armament, nation, bday);
+
+  m_head = new Node(tank);
+  Node* currNode = m_head;
+
+  if (tanksFile.is_open()) {
+    while (tanksFile >> identifier >> name >> armament >> nation >> bday) { 
+      Tank tank(identifier, name, armament, nation, bday);
+      Node* nextNode = new Node(tank);
+      currNode -> m_next = nextNode;
+      currNode = nextNode;
+    }
+  }
+
+  m_tail = currNode;
+}
+
+Node* TankList::getHead() {
+    return m_head;
+}
+
+Node* TankList::getTail() {
+    return m_tail;
+}
+
+void TankList::pushBack(const Tank newItem) {
+    //Create a new node, instantiated with the new tank object
+    Node* newNode = new Node(newItem);
+    m_tail -> m_next = newNode;
+    m_tail = newNode;
+}
+/*
+void TankList::pushBack(const Tank tankarray[], int size) {
+    TankList bufferList = TankList(tankarray, size);
+    m_tail -> m_next = bufferList.getHead();
+    m_tail = bufferList.getTail();
+}
+*/
+Tank* TankList::searchFor(string target) {
+    Node* currNode = m_head;
+    while (currNode != nullptr) {
+        //Search through each of the data fields to see if the search term matches
+        //Any of the tank's fields.
+        if (target.compare(currNode -> m_tank.getArmament())    ||
+            target.compare(currNode -> m_tank.getBday())        ||
+            target.compare(currNode -> m_tank.getIdentifier())  ||
+            target.compare(currNode -> m_tank.getName())        ||
+            target.compare(currNode -> m_tank.getNation())      ){
+                //HIT! Return the tank node
+                return &currNode -> m_tank;
+            } else {
+                currNode = currNode -> m_next;
+            }
+    }
+    //Did not find tank
+    return nullptr;
+}
