0eab96db389bb3ef65f90e85d400ca559f2a7996
Andersen-Ader <work+gh@andersenader.com>
2024-06-04 14:02:04 -0700

    Added file for math final project

---
+++ mazeFinder.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+class Node {
+  public:
+    Node();
+
+  private:
+    
+  };
+
+  Node::Node() {
+  }
+
+}
+
+int main (int argc, char *argv[]) {
+  node maze[7][7];
+  
+  return 0;
+}
