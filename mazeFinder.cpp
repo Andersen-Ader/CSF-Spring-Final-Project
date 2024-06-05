d669411fa7b0d02b142a34d41c8035461bc7363c
Andersen-Ader <work+gh@andersenader.com>
2024-06-04 21:58:07 -0700

    SCREAMING

--- mazeFinder.cpp
+++ mazeFinder.cpp
@@ -8,16 +8,45 @@ using namespace std;
 
 typedef struct Node {
 	int isBarrier = 0;
-	int Visited;
+	int Visited = 0;
+	int ownX;
+	int ownY;	
 	int north = ownX - 1;
 	int south = ownX + 1;
 	int east = ownY + 1;
 	int west = ownY - 1;
-	int ownX;
-	int ownY;	
 } Node;
 
-void CreateMaze(Node* array[][]) {
+void FindPath(Node array[SIZE_X][SIZE_Y], int curr_x, int curr_y, int end_x, int end_y, string path) {
+  if (curr_x == end_x && curr_y == end_y) {
+    cout << path;
+    return;
+  }
+
+  array[curr_x][curr_y].Visited = 1;
+
+  if (!(array[array[curr_x][curr_y].north][curr_y].isBarrier || array[array[curr_x][curr_y].north][curr_y].Visited)) {
+    cout << "Yay" << endl;
+    return;
+  }
+
+  return;
+}
+
+int main(void) {
+	Node array[SIZE_X][SIZE_Y];
+  int start_x = 1, start_y = 0;
+  int end_x = 9, end_y = 7;
+  string path;
+
+	for ( int i = 0; i < SIZE_X; ++i) {
+		for ( int j = 0; j < SIZE_Y; ++j) {
+			array[i][j].ownX = i;
+			array[i][j].ownY = j;
+		}
+	}	
+
+  
   array[0][3].isBarrier = 1;
   array[1][1].isBarrier = 1;
   array[1][2].isBarrier = 1;
@@ -25,7 +54,7 @@ void CreateMaze(Node* array[][]) {
   array[1][4].isBarrier = 1;
   array[1][7].isBarrier = 1;
   array[1][8].isBarrier = 1;
-  array[2][2].isBarrier = 1;
+  array[2][1].isBarrier = 1;
   array[2][6].isBarrier = 1;
   array[2][7].isBarrier = 1;
   array[2][8].isBarrier = 1;
@@ -56,19 +85,12 @@ void CreateMaze(Node* array[][]) {
   array[9][4].isBarrier = 1;
   array[9][6].isBarrier = 1;
   array[9][8].isBarrier = 1;
-}
 
-int main(void) {
-	Node array[SIZE_X][SIZE_Y];
 
-	for ( int i = 0; i < SIZE_X; ++i) {
-		for ( int j = 0; j < SIZE_Y; ++j) {
-			array[i][j].ownX = i;
-			array[i][j].ownY = j;
-		}
-	}	
 
-  CreateMaze(array);
+  FindPath(array, start_x, start_y, end_x, end_y, path);
+
+
 
 	for ( int i = 0; i < SIZE_X; ++i) {
 		for ( int j = 0; j < SIZE_Y; ++j) {
