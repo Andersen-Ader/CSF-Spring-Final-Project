102ab3f318629e53d4b35fc1542594cc7c5f8998
Andersen Ader <work+gh@andersenader.com>
2024-06-05 07:31:23 +0000

    Maybe better

--- mazeFinder.cpp
+++ mazeFinder.cpp
@@ -11,32 +11,49 @@ typedef struct Node {
 	int Visited = 0;
 	int ownX;
 	int ownY;	
-	int north = ownX - 1;
-	int south = ownX + 1;
-	int east = ownY + 1;
-	int west = ownY - 1;
+	int north;
+	int south;
+	int east;
+	int west;
 } Node;
 
 void FindPath(Node array[SIZE_X][SIZE_Y], int curr_x, int curr_y, int end_x, int end_y, string path) {
   if (curr_x == end_x && curr_y == end_y) {
-    cout << path;
+    cout << path << endl;
     return;
   }
 
-  Node currNode = array[curr_x][curr_y]
+  cout << curr_x << " " << curr_y << endl;
+
+  Node currNode = array[curr_x][curr_y];
 
   array[curr_x][curr_y].Visited = 1;
 
-  if (!(array[currNode.north][curr_y].isBarrier)) {
-    cout << "Yay" << endl;
-    return;
+  if (currNode.north >= 0 && array[currNode.north][curr_y].isBarrier == 0 && array[currNode.north][curr_y].Visited == 0) {
+    path.push_back('N');
+    FindPath(array, currNode.north, curr_y, end_x, end_y, path);
   }
 
+  if (currNode.south < SIZE_X && array[currNode.south][curr_y].isBarrier == 0 && array[currNode.south][curr_y].Visited == 0) {
+    path.push_back('S');
+    FindPath(array, currNode.south, curr_y, end_x, end_y, path);
+  }
+  
+  if (currNode.east < SIZE_Y && array[curr_x][currNode.east].isBarrier == 0 && array[curr_x][currNode.east].Visited == 0) {
+    path.push_back('E');
+    FindPath(array, curr_x, currNode.east, end_x, end_y, path);
+  }
+  
+  if (currNode.west >= 0 && array[curr_x][currNode.west].isBarrier == 0 && array[curr_x][currNode.west].Visited == 0) {
+    path.push_back('W');
+    FindPath(array, curr_x, currNode.west, end_x, end_y, path);
+  }
+  
   return;
 }
 
 int main(void) {
-	Node array[SIZE_X][SIZE_Y];
+  Node array[SIZE_X][SIZE_Y];
   int start_x = 1, start_y = 0;
   int end_x = 9, end_y = 7;
   string path;
@@ -45,6 +62,10 @@ int main(void) {
 		for ( int j = 0; j < SIZE_Y; ++j) {
 			array[i][j].ownX = i;
 			array[i][j].ownY = j;
+			array[i][j].north = i - 1;
+			array[i][j].south = i + 1;
+			array[i][j].east = j + 1;
+			array[i][j].west = j - 1;
 		}
 	}	
 
@@ -93,14 +114,14 @@ int main(void) {
   FindPath(array, start_x, start_y, end_x, end_y, path);
 
 
-
+/*
 	for ( int i = 0; i < SIZE_X; ++i) {
 		for ( int j = 0; j < SIZE_Y; ++j) {
       cout << array[i][j].isBarrier << " ";
 		}
     cout << endl;
   }
-
+*/
 
 	return 0;
 }
